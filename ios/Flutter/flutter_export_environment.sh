e5fa550ac08bbe83e83a952217e582e56a45fb39
Samriddhi Narayan <113301031+NarayanSam@users.noreply.github.com>
2022-10-27 17:15:27 +0530

    Add files via upload

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Login\Flutter_Login-master\Flutter_Login"
+export "FLUTTER_APPLICATION_PATH=C:\Flutter_Login"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
